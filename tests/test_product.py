017d38434f3986e466f4ec5dcdfbe00415d681fd
DmitryOta <dmitrydsm@bk.ru>
2024-09-21 23:52:10 +0300

    add tests

---
+++ tests/test_product.py
@@ -0,0 +1,6 @@
+
+def test_product(product):
+    assert product.name == "Ipad"
+    assert product.description == "Цвет серый"
+    assert product.price == 100000.0
+    assert product.quantity == 3
