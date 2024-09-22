a9931616d7841907d00db04d82e3f76f0a9f8392
DmitryOta <dmitrydsm@bk.ru>
2024-09-22 11:26:26 +0300

    dell

--- tests/test_category.py
+++
@@ -1,7 +0,0 @@
-def test_category(category_one, category_two):
-    assert category_one.name == "Телефоны"
-    assert category_one.description == "Телефон на каждый день"
-    assert len(category_one.products) == 2
-
-    assert category_two.count_category == 2
-    assert category_two.count_product == 2
